b3e365c1f1730651b3256349d471a5d39a054708
hyckomatej <158080691+hyckomatej@users.noreply.github.com>
2025-06-26 14:50:58 +0200

    Remove afl metrics (#11)

--- src/fuzzing_metrics/mod.rs
+++
@@ -1,4 +0,0 @@
-pub(crate) mod error_entry;
-pub(crate) mod serializer;
-pub mod stats;
-pub(crate) mod transaction_slot;
--- src/methods/mod.rs
+++ src/methods/mod.rs
@@ -1,4 +1,3 @@
 mod trident_svm_accounts;
-mod trident_svm_metrics;
 mod trident_svm_programs;
 mod trident_svm_transactions;
