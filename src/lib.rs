0237cbeaa6375a300896d67e8c29a7bd67fb7710
andrej <andrej@andrejs-MacBook-Pro.local>
2025-07-13 21:05:55 +0200

    ✨ Use syscall features

--- src/lib.rs
+++ src/lib.rs
@@ -6,16 +6,20 @@ mod trident_fork_graphs;
 pub mod trident_svm_log;
 mod utils;
 
+#[cfg(any(feature = "syscall-v1", feature = "syscall-v2"))]
 pub mod builtin_function;
 pub mod trident_svm;
 pub mod types;
 
 pub mod processor {
+    #[cfg(any(feature = "syscall-v1", feature = "syscall-v2"))]
     pub use crate::builtin_function::post_invocation;
+    #[cfg(any(feature = "syscall-v1", feature = "syscall-v2"))]
     pub use crate::builtin_function::pre_invocation;
 
     pub use solana_program_runtime;
     pub use solana_program_runtime::stable_log;
+    #[cfg(any(feature = "syscall-v1", feature = "syscall-v2"))]
     pub use solana_rbpf;
     pub use solana_sdk::account_info;
     pub use solana_sdk::entrypoint::deserialize;
--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,3 +1,4 @@
 pub mod trident_account;
+#[cfg(any(feature = "syscall-v1", feature = "syscall-v2"))]
 pub mod trident_entrypoint;
 pub mod trident_program;
