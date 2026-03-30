1945b4e8f8ea7a0bae5d58e7ddfd436bcdbc95c6
lukacan <andrej.lukacovic@ackee.xyz>
2026-03-30 16:23:09 +0200

    🐛 Improve transaction results

--- src/methods/trident_svm_transactions.rs
+++ src/methods/trident_svm_transactions.rs
@@ -14,114 +14,104 @@ use solana_svm::transaction_processor::TransactionProcessingConfig;
 use solana_svm::transaction_processor::TransactionProcessingEnvironment;
 
 use crate::trident_svm::TridentSVM;
-use crate::types::transaction_result::TridentTransactionProcessingResult;
+use crate::types::transaction_result::TridentTransactionResult;
+
+const LOG_MESSAGES_BYTES_LIMIT: usize = 100 * 1000;
+
+fn recording_config() -> ExecutionRecordingConfig {
+    ExecutionRecordingConfig {
+        enable_cpi_recording: true,
+        enable_log_recording: true,
+        enable_return_data_recording: true,
+        enable_transaction_balance_recording: false,
+    }
+}
 
 impl TridentSVM {
-    pub fn process_transaction(
-        &mut self,
-        transaction: Transaction,
-    ) -> TridentTransactionProcessingResult {
-        let tx_processing_environment = TransactionProcessingEnvironment::<'_> {
-            feature_set: *self.feature_set,
-            ..Default::default()
-        };
-
-        let tx_processing_config = TransactionProcessingConfig::default();
-
-        // reset sysvar cache
-        self.processor.reset_sysvar_cache();
-
-        // replenish sysvar cache with sysvars from the accounts db
-        self.processor.fill_missing_sysvar_cache_entries(self);
-
-        // create sanitized transaction
-        let sanitezed_tx =
-            SanitizedTransaction::try_from_legacy_transaction(transaction, &HashSet::new())
-                .unwrap();
-
-        // get current transaction timestamp
-        let transaction_timestamp =
-            self.accounts.deserialize_sysvar::<Clock>().unix_timestamp as u64;
-
-        // execute transaction
-        let res = self.processor.load_and_execute_sanitized_transactions(
-            self,
-            &[sanitezed_tx],
-            get_transaction_check_results(1),
-            &tx_processing_environment,
-            &tx_processing_config,
-        );
-
-        // update clock
-        self.accounts.update_clock();
-
-        // return transaction processing result
-        TridentTransactionProcessingResult::new(res, transaction_timestamp)
-    }
-    pub fn process_transaction_with_settle(
-        &mut self,
-        transaction: Transaction,
-    ) -> TridentTransactionProcessingResult {
+    pub fn process_transaction(&mut self, transaction: Transaction) -> TridentTransactionResult {
         let tx_processing_environment = TransactionProcessingEnvironment::<'_> {
             feature_set: *self.feature_set,
             ..Default::default()
         };
 
         let tx_processing_config = TransactionProcessingConfig {
-            log_messages_bytes_limit: Some(20 * 1000),
-            recording_config: ExecutionRecordingConfig::new_single_setting(true),
+            log_messages_bytes_limit: Some(LOG_MESSAGES_BYTES_LIMIT),
+            recording_config: recording_config(),
             ..Default::default()
         };
 
-        // reset sysvar cache
         self.processor.reset_sysvar_cache();
-
-        // replenish sysvar cache with sysvars from the accounts db
         self.processor.fill_missing_sysvar_cache_entries(self);
 
-        // create sanitized transaction
-        let sanitezed_tx =
+        let sanitized_tx =
             SanitizedTransaction::try_from_legacy_transaction(transaction, &HashSet::new())
-                .expect("Trident SVM is not able to create sanitized transaction");
+                .expect("Failed to create sanitized transaction");
 
-        // get current transaction timestamp
-        let transaction_timestamp =
-            self.accounts.deserialize_sysvar::<Clock>().unix_timestamp as u64;
+        let transaction_timestamp = self.accounts.deserialize_sysvar::<Clock>().unix_timestamp;
 
-        // execute transaction
-        let result = self.processor.load_and_execute_sanitized_transactions(
+        let output = self.processor.load_and_execute_sanitized_transactions(
             self,
-            &[sanitezed_tx],
+            &[sanitized_tx],
             get_transaction_check_results(1),
             &tx_processing_environment,
             &tx_processing_config,
         );
 
-        // update clock
         self.accounts.update_clock();
 
-        let processed_transaction = result.processing_results[0]
+        TridentTransactionResult::from_svm_output(output, transaction_timestamp)
+    }
+
+    pub fn process_transaction_with_settle(
+        &mut self,
+        transaction: Transaction,
+    ) -> TridentTransactionResult {
+        let tx_processing_environment = TransactionProcessingEnvironment::<'_> {
+            feature_set: *self.feature_set,
+            ..Default::default()
+        };
+
+        let tx_processing_config = TransactionProcessingConfig {
+            log_messages_bytes_limit: Some(LOG_MESSAGES_BYTES_LIMIT),
+            recording_config: recording_config(),
+            ..Default::default()
+        };
+
+        self.processor.reset_sysvar_cache();
+        self.processor.fill_missing_sysvar_cache_entries(self);
+
+        let sanitized_tx =
+            SanitizedTransaction::try_from_legacy_transaction(transaction, &HashSet::new())
+                .expect("Failed to create sanitized transaction");
+
+        let transaction_timestamp = self.accounts.deserialize_sysvar::<Clock>().unix_timestamp;
+
+        let output = self.processor.load_and_execute_sanitized_transactions(
+            self,
+            &[sanitized_tx],
+            get_transaction_check_results(1),
+            &tx_processing_environment,
+            &tx_processing_config,
+        );
+
+        self.accounts.update_clock();
+
+        let processed_transaction = output.processing_results[0]
             .processed_transaction()
             .expect("Transaction was not processed");
 
         match &processed_transaction {
             solana_svm::transaction_processing_result::ProcessedTransaction::Executed(
                 executed_tx,
-            ) => match &executed_tx.execution_details.status {
-                Ok(()) => {
+            ) => {
+                if executed_tx.execution_details.status.is_ok() {
                     self.settle_accounts(&executed_tx.loaded_transaction.accounts);
                 }
-                Err(_transaction_error) => {
-                    // in case of transaction error, we don't need to do anything
-                }
-            },
-            solana_svm::transaction_processing_result::ProcessedTransaction::FeesOnly(
-                _transaction_error,
-            ) => {
-                // in case of transaction error, we don't need to do anything
             }
+            solana_svm::transaction_processing_result::ProcessedTransaction::FeesOnly(_) => {}
         }
-        TridentTransactionProcessingResult::new(result, transaction_timestamp)
+
+        TridentTransactionResult::from_svm_output(output, transaction_timestamp)
     }
 }
 
@@ -129,7 +119,7 @@ pub(crate) fn get_transaction_check_results(
     len: usize,
 ) -> Vec<solana_transaction_error::TransactionResult<CheckedTransactionDetails>> {
     let compute_budget_limit = ComputeBudgetLimits {
-        updated_heap_bytes: 256 * 1024, // max heap frame
+        updated_heap_bytes: 256 * 1024,
         ..ComputeBudgetLimits::default()
     };
     vec![
